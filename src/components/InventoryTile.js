77b7a38dec34b2c8173e0e432a6f04c6be52e27d
Hiroki Kato <hirokikato1@gmail.com>
2023-08-04 12:42:39 -0400

    creates PotionTile component abstracting callback function passed as prop, removing duplicate code in  ShopTile and InventoryTile

--- src/components/InventoryTile.js
+++ src/components/InventoryTile.js
@@ -1,17 +1,17 @@
-import React from "react";
+// import React from "react";
 
-// Study me! What information do I require to work?
-// How am I the same as ShopTile? How am I different?
+// // Study me! What information do I require to work?
+// // How am I the same as ShopTile? How am I different?
 
-function InventoryTile({ potion, sell, children }) {
-  return (
-    <div className="inventory-card">
-      <div onClick={() => sell(potion.id)} className="image-wrapper">
-        <img className="image" alt={potion.name} src={potion.image_url} />
-      </div>
-      {children}
-    </div>
-  );
-}
+// function InventoryTile({ potion, sell, children }) {
+//   return (
+//     <div className="inventory-card">
+//       <div onClick={() => sell(potion.id)} className="image-wrapper">
+//         <img className="image" alt={potion.name} src={potion.image_url} />
+//       </div>
+//       {children}
+//     </div>
+//   );
+// }
 
-export default InventoryTile;
+// export default InventoryTile;
--- src/components/ShopTile.js
+++ src/components/ShopTile.js
@@ -1,17 +1,17 @@
-import React from "react";
+// import React from "react";
 
-// Study me! What information do I require to work?
-// How am I the same as InventoryTile? How am I different?
+// // Study me! What information do I require to work?
+// // How am I the same as InventoryTile? How am I different?
 
-function ShopTile({ potion, addToInventory, children }) {
-  return (
-    <div className="card">
-      <div onClick={() => addToInventory(potion.id)} className="image-wrapper">
-        <img className="image" alt={potion.name} src={potion.image_url} />
-      </div>
-      {children}
-    </div>
-  );
-}
+// function ShopTile({ potion, addToInventory, children }) {
+//   return (
+//     <div className="card">
+//       <div onClick={() => addToInventory(potion.id)} className="image-wrapper">
+//         <img className="image" alt={potion.name} src={potion.image_url} />
+//       </div>
+//       {children}
+//     </div>
+//   );
+// }
 
-export default ShopTile;
+// export default ShopTile;
